b03dee9f2db4ff84c36a398216ebc629ce786596
Austin Eng <aeng@ctemc.org>
2013-08-06 21:15:35 +0100

    made sidebar slide in and out with button click

--- js/main.js
+++ js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
         $("#feed").fadeOut();
         $("#directions").fadeIn();
     });
+    $("#shrink-arrow").click(function() {
+        /*$(this).innerHTML='&#59237;';*/
+        var type=$(this).data('type');
+        if(type=='close') {
+            $(this).data('type', 'open');
+            $(this).html('&#59237;');
+            $("#map-content").css('width','100%');
+        } else {
+            $(this).data('type', 'close');
+            $(this).html('&#59238;');
+            $("#map-content").css('width','');
+        }
+    });
 });
