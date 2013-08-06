028521022d373914d98562b7d19cf711fa56632e
amillman <amillman@ctemc.org>
2013-08-06 21:01:10 +0100

    home styling, removed report from home page

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ $(document).ready(function() {
         $("#feed").fadeOut();
         $("#directions").fadeIn();
     });
+
+    $("#feed-btn").click(function() {
+        $("#directions").hide();
+        $("#feed").fadeIn();
+    });
+
+    $(".nav-tabs > li").click(function() {
+
+        if(!$(this).hasClass("active")){
+            var newId = "#" + $(this).attr("id") + "-content";
+            $(".nav-tabs > li").removeClass("active");
+            $(this).addClass("active");
+            $(".tabcontent > div").hide();
+            $(newId).fadeIn();
+        }
+    });
 });
