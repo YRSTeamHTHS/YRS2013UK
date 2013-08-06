078e4688ed56feaccd48b53b1468e3f7ebb5a83c
Austin Eng <aeng@ctemc.org>
2013-08-06 16:27:17 +0100

    added live feed and main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$("#feed-btn").click(function() {
+    alert(2);
+    /*$("#directions").hide();
+    $("#feed").show();*/
+});
+$("#dir-btn").click(function() {
+    alert(1);
+    /*$("#feed").hide();
+    $("#directions").show();*/
+});
